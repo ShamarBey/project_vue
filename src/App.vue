ebf4957108eb97dfa008fad6dc5c4edda2578318
Alim Rizaev <rizaev.alim@icloud.com>
2025-04-15 15:38:52 +0300

    2.4 Верстка меню в дровере

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,30 @@
 <template>
   <v-app>
-    <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
+    <!-- Боковое меню -->
+    <v-navigation-drawer app v-model="drawer">
+      <!-- Первый блок: заголовок и подзаголовок -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> КИПУ </v-list-item-title>
+          <v-list-item-subtitle> Учебный проект </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <!-- Разделитель -->
+      <v-divider></v-divider>
+
+      <!-- Навигационное меню -->
+      <v-list dense>
+        <v-list-item>
+          <template v-slot:prepend>
+            <v-icon icon="mdi-cake-variant"></v-icon>
+          </template>
+          <v-list-item-title>Link One</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Верхняя панель -->
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
@@ -13,11 +37,13 @@
         <v-btn>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
+
     <v-content>
       <router-view></router-view>
     </v-content>
   </v-app>
 </template>
+
 <script>
 export default {
   data() {
