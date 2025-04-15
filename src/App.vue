98c16baa0672fc8d7f601c9cdd844ff81b0b9622
Alim Rizaev <rizaev.alim@icloud.com>
2025-04-15 15:40:41 +0300

    2.5 Создание динамического меню

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <v-app>
-    <!-- Боковое меню -->
     <v-navigation-drawer app v-model="drawer">
-      <!-- Первый блок: заголовок и подзаголовок -->
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title"> КИПУ </v-list-item-title>
@@ -10,37 +8,33 @@
         </v-list-item-content>
       </v-list-item>
 
-      <!-- Разделитель -->
       <v-divider></v-divider>
 
-      <!-- Навигационное меню -->
       <v-list dense>
-        <v-list-item>
+        <v-list-item v-for="link in links" :key="link.title">
           <template v-slot:prepend>
-            <v-icon icon="mdi-cake-variant"></v-icon>
+            <v-icon :icon="link.icon"></v-icon>
           </template>
-          <v-list-item-title>Link One</v-list-item-title>
+          <v-list-item-title>{{ link.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
-    <!-- Верхняя панель -->
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
+
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn>
-          <v-icon start icon="mdi-cake-variant"></v-icon>
-          Link One
+        <v-btn text v-for="link in links" :key="link.title">
+          <v-icon start :icon="link.icon"></v-icon>
+          {{ link.title }}
         </v-btn>
-        <v-btn>Link Two</v-btn>
-        <v-btn>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
 
-    <v-content>
+    <v-main>
       <router-view></router-view>
-    </v-content>
+    </v-main>
   </v-app>
 </template>
 
@@ -49,6 +43,17 @@ export default {
   data() {
     return {
       drawer: false,
+      links: [
+        { title: "Login", icon: "mdi-lock", url: "/login" },
+        { title: "Registration", icon: "mdi-face", url: "/registration" },
+        {
+          title: "Orders",
+          icon: "mdi-bookmark-multiple-outline",
+          url: "/orders",
+        },
+        { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
+        { title: "My ads", icon: "mdi-view-list-outline", url: "/list" },
+      ],
     };
   },
 };
