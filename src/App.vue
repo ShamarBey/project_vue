667c3ecf905e1b7bd593c8d67eecc057f1f3b733
Alim Rizaev <rizaev.alim@icloud.com>
2025-04-15 15:34:00 +0300

    2.1 СОЗДАНИЕ ШАБЛОНА (LAYOUT)

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app>
+    <v-navigation-drawer app></v-navigation-drawer>
+    <v-app-bar app dark color="primary">
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn text>Link One</v-btn>
+        <v-btn text>Link Two</v-btn>
+        <v-btn text>Link Three</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-main> </v-main>
+  </v-app>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
